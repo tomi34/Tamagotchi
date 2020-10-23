6e4149e64965c2157d57f2fea4de234260de1ef1
tomi34 <66636695+tomi34@users.noreply.github.com>
2020-10-23 10:34:24 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
- 
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    while (controller.A.isPressed()) {
+    	
+    }
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . c c 
+    . . . c 4 d 4 4 4 4 4 1 c c 4 c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
+    f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
+    . f 4 4 4 4 1 c c 4 4 d f f . . 
+    . . f f 4 d 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
